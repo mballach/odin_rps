40dbd3f298c61ecc720e5440ebe3a9cf076c8fa0
mballach <muggins111@live.ca>
2024-10-28 12:31:08 -0700

    Add logic for player choice selection

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log('hello world')
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let cpuChoice = Math.random();
     if (cpuChoice < 0.33333333) {
@@ -10,5 +13,25 @@ function getComputerChoice() {
         return("scissors")
     }
 }
+
+
+function getHumanChoice() {
+    let humChoice= prompt('Please enter "rock", "paper", or "scissors" to select your choice');
+    humChoice = humChoice.toLowerCase();
+    if (humChoice == "rock" || humChoice == "r") {
+        console.log("You have selected rock");
+        return "rock"
+    } else if (humChoice == "paper"|| humChoice == "p") {
+        console.log("You have selected paper");
+        return "paper"
+    } else if (humChoice == "scissors"|| humChoice == "s") {
+        console.log("You have selected scissors")
+        return "scissors"
+    } else {
+        console.log("Please enter a valid selection")
+        return getHumanChoice()
+    }
+}
+
 
 
