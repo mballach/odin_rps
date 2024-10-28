6080f49c4976e0b3a5d5acec7f7f01af514d85da
mballach <muggins111@live.ca>
2024-10-28 13:02:25 -0700

    Add logic for playing a full game with 5 rounds

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-console.log('hello world')
-
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let cpuChoice = Math.random();
     if (cpuChoice < 0.33333333) {
@@ -33,46 +28,64 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    console.log("Computer has selected " +computerChoice)
-    let result = humanChoice+computerChoice;
-
-    switch (result) {
-        case "rockrock":
-        case "paperpaper":
-        case "scissorsscissors":
-            console.log("It's a tie! You both selected "+humanChoice);
-            break;
-        case "rockpaper":
-            console.log("You selected rock and computer selected paper, you lose!");
-            computerScore++;
-            break;
-        case "rockscissors":
-            console.log("You selected rock and computer selected scissors, you win!");
-            humanScore++;
-            break;
-        case "paperrock":
-            console.log("You selected paper and computer selected rock, you win!");
-            humanScore++;
-            break;
-        case "paperscissors":
-            console.log("You selected paper and computer selected scissors, you lose!");
-            computerScore++;
-            break;
-        case "scissorsrock":
-            console.log("You selected scissors and computer selected rock, you lose!");
-            computerScore++;
-            break;
-        case "scissorspaper":
-            console.log("You selected scissors and computer selected paper, you win!");
-            humanScore++;
-            break;
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    let roundNumber = 0;
+    
+    function playRound(humanChoice, computerChoice) {
+        console.log("Computer has selected " +computerChoice)
+        let result = humanChoice+computerChoice;
+        switch (result) {
+            case "rockrock":
+            case "paperpaper":
+            case "scissorsscissors":
+                console.log("It's a tie! You both selected "+humanChoice);
+                break;
+            case "rockpaper":
+                console.log("You selected rock and computer selected paper, you lose!");
+                computerScore++;
+                break;
+            case "rockscissors":
+                console.log("You selected rock and computer selected scissors, you win!");
+                humanScore++;
+                break;
+            case "paperrock":
+                console.log("You selected paper and computer selected rock, you win!");
+                humanScore++;
+                break;
+            case "paperscissors":
+                console.log("You selected paper and computer selected scissors, you lose!");
+                computerScore++;
+                break;
+            case "scissorsrock":
+                console.log("You selected scissors and computer selected rock, you lose!");
+                computerScore++;
+                break;
+            case "scissorspaper":
+                console.log("You selected scissors and computer selected paper, you win!");
+                humanScore++;
+                break;
+        }
+        console.log(`Your score: ${humanScore}  -  Computer score: ${computerScore}`);
     }
-    console.log(`Your score: ${humanScore}  -  Computer score: ${computerScore}`);
+
+    while (roundNumber < 5) {
+        playRound(getHumanChoice(),getComputerChoice());
+        roundNumber++;
+    }
+
+    console.log(`The game is over! You finished with ${humanScore} round win(s), and the computer finished with ${computerScore} win(s).`)
+    if (humanScore==computerScore) {
+        console.log("It was a tie!");
+    } else if (humanScore > computerScore) {
+        console.log("You win!");
+    } else {
+        console.log("The computer wins!");
+    }
+
 }
 
-/*const humanSelection = getHumanChoice();
-const computerSelectoin = getComputerChoice();
-*/
+
 
 
